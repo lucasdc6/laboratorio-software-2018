5f30eed321fd0e4045b85d7de97a119a4175df1f
lucasdc6 <lucasdicunzolo@gmail.com>
2018-12-04 14:40:14 -0300

    Added files

---
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Recognize.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.cedica.razasypelajescedica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Recognize extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognize);
+    }
+}

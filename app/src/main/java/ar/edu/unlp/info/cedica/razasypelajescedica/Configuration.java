2455c48c2224a886b3d5de178101bf9722642239
lucasdc6 <lucasdicunzolo@gmail.com>
2018-12-09 18:18:15 -0300

    Added filew

---
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Configuration.java
@@ -0,0 +1,101 @@
+package ar.edu.unlp.info.cedica.razasypelajescedica;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.view.View;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Configuration extends AppCompatActivity
+{
+
+    private Switch levelSwitch;
+    private Switch sexSwitch;
+    private RadioGroup minigameRadio;
+    private RadioGroup interactionModeRadio;
+    private RadioGroup viewModeRadio;
+    private HashMap<String, CheckBox> gameModeCheckBoxMap;
+    private  SharedPreferences preferences;
+
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize variables
+        gameModeCheckBoxMap = new HashMap<>();
+
+        preferences = getSharedPreferences("config_preferences", Context.MODE_PRIVATE);
+
+         setContentView(R.layout.activity_configuration);
+
+        // Initialize configurations
+        initConfig();
+     }
+
+     public void onAccept(View view) {
+         HashSet<String> selectedGameModes = new HashSet<>();
+
+         SharedPreferences.Editor editor = preferences.edit();
+
+         editor.putBoolean("level", levelSwitch.isChecked());
+         editor.putBoolean("sex", sexSwitch.isChecked());
+
+         editor.putInt("minigame", minigameRadio.getCheckedRadioButtonId());
+         editor.putInt("viewMode", viewModeRadio.getCheckedRadioButtonId());
+         editor.putInt("interactionMode", interactionModeRadio.getCheckedRadioButtonId());
+
+         for (Map.Entry<String, CheckBox> entry : gameModeCheckBoxMap.entrySet()) {
+             if (entry.getValue().isChecked()) {
+                 selectedGameModes.add(entry.getKey());
+             }
+
+         }
+
+         editor.putStringSet("gameModes", selectedGameModes);
+         editor.apply();
+         finish();
+     }
+
+     private void initConfig() {
+         levelSwitch = findViewById(R.id.Level);
+         sexSwitch = findViewById(R.id.Sex);
+         minigameRadio = findViewById(R.id.Minigame);
+         interactionModeRadio = findViewById(R.id.InteractionMode);
+         viewModeRadio = findViewById(R.id.ViewMode);
+
+         gameModeCheckBoxMap.put("RacesCheckbox", (CheckBox) findViewById(R.id.RacesCheckbox));
+         gameModeCheckBoxMap.put("CoatsCheckbox", (CheckBox) findViewById(R.id.CoatsCheckbox));
+         gameModeCheckBoxMap.put("CrossesCheckbox", (CheckBox) findViewById(R.id.CrossesCheckbox));
+
+         levelSwitch.setChecked(preferences.getBoolean("level", false));
+         sexSwitch.setChecked(preferences.getBoolean("sex", false));
+         minigameRadio.check(preferences.getInt("minigame", R.id.MinigameRacesCoats));
+         viewModeRadio.check(preferences.getInt("viewMode", R.id.RecognizeModeList));
+         interactionModeRadio.check(preferences.getInt("interactionMode", R.id.InteractionModeB));
+
+         for (CheckBox checkBox : gameModeCheckBoxMap.values()) {
+             checkBox.setChecked(false);
+         }
+
+         Set<String> gameModes = preferences.getStringSet("gameModes", new HashSet<String>());
+
+        if (gameModes == null) {
+            return;
+        }
+
+        for (String checkBoxKey : gameModes) {
+            CheckBox checkBox = gameModeCheckBoxMap.get(checkBoxKey);
+            if (checkBox != null) {
+                checkBox.setChecked(true);
+            }
+        }
+     }
+}
--- app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Main.java
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Main.java
@@ -1,22 +1,20 @@
 package ar.edu.unlp.info.cedica.razasypelajescedica;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+    protected void toConfig(View view) {
+        Intent intent = new Intent(this, Configuration.class);
+        startActivity(intent);
     }
 
 }

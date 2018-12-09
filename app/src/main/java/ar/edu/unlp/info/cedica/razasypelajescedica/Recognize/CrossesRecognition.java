7d47b4c70fcd88dae2b014d0226a19841552420e
lucasdc6 <lucasdicunzolo@gmail.com>
2018-12-09 19:28:03 -0300

    Added minigames scaffold

---
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Recognize/CrossesRecognition.java
@@ -0,0 +1,17 @@
+package ar.edu.unlp.info.cedica.razasypelajescedica.Recognize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ar.edu.unlp.info.cedica.razasypelajescedica.R;
+
+
+public class CrossesRecognition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_crosses_recognition);
+    }
+}
---
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Recognize/RacesAndCoatsRecognition.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.cedica.razasypelajescedica.Recognize;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ar.edu.unlp.info.cedica.razasypelajescedica.R;
+
+public class RacesAndCoatsRecognition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_races_and_coats_recognition);
+    }
+}

fe0f042cfb2ff026b723ab78156aeb8603657eb4
ulises-jeremias <ulisescf.24@gmail.com>
2018-12-09 19:43:36 -0300

    Added main logi

--- app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Main.java
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Main.java
@@ -15,22 +15,27 @@ import ar.edu.unlp.info.cedica.razasypelajescedica.recognize.CrossesRecognition;
 import java.util.ArrayList;
 
 public class Main extends AppCompatActivity {
+    private ArrayList<Class<? extends AppCompatActivity>> recognitionModes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        recognitionModes =
+                new ArrayList<Class<? extends  AppCompatActivity>>();
+
+        recognitionModes.add(RacesCoatsRecognition.class);
+        recognitionModes.add(RacesAndCoatsRecognition.class);
+        recognitionModes.add(CrossesRecognition.class);
+
         setContentView(R.layout.activity_main);
     }
 
     public void onPlayButtonClick() {
         SharedPreferences preferences = getSharedPreferences("config_preferences", Context.MODE_PRIVATE);
-        Intent intent;
+        Intent intent = new Intent(this, recognitionModes.get(0));
 
-        ArrayList<Class<? extends AppCompatActivity>> recognitionModes =
-            new ArrayList<Class<? extends  AppCompatActivity>>();
-
-        recognitionModes.add(RacesCoatsRecognition.class);
-        recognitionModes.add(RacesAndCoatsRecognition.class);
-        recognitionModes.add(CrossesRecognition.class);
+        startActivity(intent);
     }
 
     protected void toConfig(View view) {

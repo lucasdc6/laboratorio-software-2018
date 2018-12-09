85f7cd0f0389f4a99d0dbe2d8c12d3aaf49336c2
ulises-jeremias <ulisescf.24@gmail.com>
2018-12-09 16:17:47 -0300

    Updated Main.java

--- app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Main.java
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Main.java
@@ -11,26 +11,6 @@ public class Main extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

d9c4f194b6b2bdb158bafd0c70acfb36eb8737b6
ulises-jeremias <ulisescf.24@gmail.com>
2018-12-10 01:04:29 -0300

    Se agrega barra de scroll

--- app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Help.java
+++ app/src/main/java/ar/edu/unlp/info/cedica/razasypelajescedica/Help.java
@@ -3,7 +3,9 @@ package ar.edu.unlp.info.cedica.razasypelajescedica;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
 public class Help extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class Help extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_help);
+        ((TextView) findViewById(R.id.HelpText))
+                .setMovementMethod(new ScrollingMovementMethod());
     }
 
     public void goBack(View view) {
